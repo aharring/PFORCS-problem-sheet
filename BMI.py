1d1135149af36cd6612331a6fc5f97672d838906
Adele Harrington <adele.harrington@gmail.com>
2021-02-01 12:29:52 +0000

    Adjustments made to cater for input in cm as opposed to metres. Some error checks added to try catch out of bounds heights. It is possible to enter either 180 or 1.8 and it will calculate the correct result

--- BMI.py
+++ BMI.py
@@ -6,11 +6,21 @@
 # This program prompts the user for their Weight & Height 
 # In both cases it does a simple error check to verify that the input evaluates 
 # to a float, just to avoid obvious errors like someone entering a non digit
-# 
+#
+# Niall pointed out the height is expected in cm.
+# So I have attempted to do some error checking around the number entered to avoid an unrealistic answer
+# I chose 272 as an upper limit for converting to m bc the tallest ever recorded height is 2.72metres
+# I chose 24 as the lower limit for converting from cm to m bc guinness book of records lists 24cm as shortest ever recorded 
+#
+# If the input was between .24 and 2.72 I assume the value entered is already in m, tell the user and calculate accordingly
+#
+# If the input is greater than the tallest person in metres (2.72) or cm (272) or smaller than the smallest person in cm (24) 
+# or entered a negative number just to see then I print a message indicating I think there is a problem with the height entered
+#
+# I make no assumptions about weight
+#
 # It then calculates their BMI using the formula kg/hsquared 
-# and outputs the result
-
-IsAFloat = False # A boolean to help with the while loop
+# and outputs the result rounded to 2 decimal places
 
 # First prompt the user to enter their weight
 # A simple check is done to see if the value entered evaluates to a float
@@ -23,12 +33,13 @@ IsAFloat = False # A boolean to help with the while loop
 # of suggestions came up but this looked like the best for what I was trying to 
 # accomplish
 
+BreakOutOfLoop = False # A boolean to help with the while loop
 
-while IsAFloat == False : 		# First time in this variable is False so we enter loop
-    print ("Please enter weight :") 	# Execute print statement
+while BreakOutOfLoop == False : 	# First time in this variable is False so we enter loop
+    print ("Please enter weight in Kg :") 	# Execute print statement
     try :
         Weight = float(input()) 	# Assign the input to variable Weight. If the datatype can evaluate to a float Try has worked
-        IsAFloat = True         	# If the try has worked IsAFloat is set to True and we can break out of our While
+        BreakOutOfLoop = True         	# If the try has worked IsAFloat is set to True and we can break out of our While
     except:
         print("Weight should be entered as a number ")  # If Weight = float(input()) gave an error this statement is executed
         continue 					# Back up to the top of the loop and execute bc IsAFloat will still be False
@@ -36,22 +47,31 @@ while IsAFloat == False : 		# First time in this variable is False so we enter l
 # Reset the test variable IsAFloat because we are now going to do a similar test
 # on the Height input
 
-IsAFloat = False
+BreakOutOfLoop = False
 
-while IsAFloat == False :
-    print ("Please enter height :")
+while BreakOutOfLoop  == False :
+    print ("Please enter height (cm) :")
     try :
         Height = float(input())
-        IsAFloat = True 
     except:
         print("Height should be entered as a number ")
         continue
+
+    if (Height >= 24) and (Height <=272): # If the height is between 24 and 272 assume user entered cm and divide by 100 for m
+        Height = Height/100 	# Divide input by 100 and reassign it to Height
+        BreakOutOfLoop = True 	# If yes exit the loop
+    elif (Height >= .24) and (Height <= 2.72):
+        print ("Based on input value assuming the height was entered in metres not cm. Calculating accordingly")
+        BreakOutOfLoop = True
+    elif  (Height > 2.72 and Height < 24) or (Height > 272) or (Height <= .24 ): # Trying to catch unrealistic entries
+         print("Please reconfirm your height")
+         BreakOutOfLoop = False	
 
 # Calculate the square of height & reassign it to the variable Height
 Height = Height ** 2 # Set the variable Height to Height squared
 
 # Calculate the BMI by dividing Weight by Height. Remember Height here contains the squared value
-BMI = Weight/Height
+BMI = round((Weight/Height), 2) # Ed pointed out it might be tidier to round the answer
 
 # Print the string 'Your BMI is' followed by the string conversion of the BMI value previously
 print ("Your BMI is " + str(BMI))
